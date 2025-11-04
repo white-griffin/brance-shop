a9dccfb37d7b4d0b8a959f5fc07df08efcb262c2
amin <mohamadamin.zanguee@gmail.com>
2025-11-04 13:55:04 +0330

    add new changes

--- app/Http/Resources/ProductResource.php
+++ app/Http/Resources/ProductResource.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Resources;
 
-use App\Constants\Constant;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use App\Constants\Constant;
+use App\Helpers\Format\Date;
+use App\Http\Resources\{CommentResource, ProductAttributesResource};
 
 class ProductResource extends JsonResource
 {
@@ -35,6 +37,7 @@ class ProductResource extends JsonResource
             'canonical_url' => $this->canonical_url,
             'category_name' => $this->category->title,
             'category_slug' => $this->category->slug,
+            'update_date' => Date::toJalaliFormat($this->updated_at),
         ];
     }
 
@@ -66,6 +69,12 @@ class ProductResource extends JsonResource
             'meta_title' => strip_tags($category->meta_title),
             'meta_description' => strip_tags($category->meta_description),
             'canonical_url' => $category->canonical_url,
+            'parent_category' => !is_null($category->parent)?
+                [
+                    'title' => $category->parent,
+                    'slug' => $category->parent
+                ]
+                : null,
         ];
     }
 }
